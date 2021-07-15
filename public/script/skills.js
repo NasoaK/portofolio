314ad8dabf1a67cd4c14b08cc484433aef70a50b
Mikael Kombia <mikael.kombia@gmail.com>
2021-07-16 00:03:11 +0200

    test effect
    revoir l'effect  // extremement moche

--- public/script/skills.js
+++ public/script/skills.js
@@ -1,14 +1,14 @@
 const skills = [
-    {name:"HTML 5", img:"assets/techno-icon/icons8-html-5-100.png"},
-    {name:"CSS", img:"assets/techno-icon/icons8-css3-100.png"},
-    {name:"Javascript", img:"assets/techno-icon/icons8-logo-javascript-100.png"},
-    {name:"Angular", img:"assets/techno-icon/icons8-angularjs-100.png"},
-    {name:"Jquery", img:"assets/techno-icon/icons8-jquery-100.png"},
-    {name:"php", img:"assets/techno-icon/icons8-logo-php-100.png"},
-    {name:"responsive", img:"assets/techno-icon/icons8-requêtes-médias-100.png"},
-    {name:"figma", img:"assets/techno-icon/icons8-figma-100.png"},
-    {name:"mySQL", img:"assets/techno-icon/icons8-logo-de-mysql-100.png"},
-    {name:"laravel", img:"assets/techno-icon/icons8-laravel-100.png"}
+    {name:"HTML 5", img:"assets/techno-icon/html5.png",img2:"assets/techno-icon/html5-2.png"},
+    {name:"CSS", img:"assets/techno-icon/css.png",img2:"assets/techno-icon/css-2.png"},
+    {name:"Javascript", img:"assets/techno-icon/js.png",img2:"assets/techno-icon/js-2.png"},
+    {name:"Angular", img:"assets/techno-icon/angular.png",img2:"assets/techno-icon/angular-2.png"},
+    {name:"Jquery", img:"assets/techno-icon/jquery.png",img2:"assets/techno-icon/jquery-2.png"},
+    {name:"php", img:"assets/techno-icon/php.png",img2:"assets/techno-icon/php-2.png"},
+    {name:"responsive", img:"assets/techno-icon/responsive.png",img2:"assets/techno-icon/responsive-2.png"},
+    {name:"figma", img:"assets/techno-icon/figma.png",img2:"assets/techno-icon/figma-2.png"},
+    {name:"mySQL", img:"assets/techno-icon/mysql.png",img2:"assets/techno-icon/mysql-2.png"},
+    {name:"laravel", img:"assets/techno-icon/laravel.png",img2:"assets/techno-icon/laravel-2.png"}
 
 ];
 
@@ -20,7 +20,8 @@ const skillMock = document.querySelector('.skill');
     const skillClone = skillMock.cloneNode(true);
     skills_list.append(skillClone);
     skillClone.querySelector('p').innerText = skill.name;
-    skillClone.querySelector('img').src = skill.img;
+    skillClone.querySelector('.skills-img').src = skill.img;
+    skillClone.querySelector('.skills-img-2').src = skill.img2 
  }
    
  skillMock.remove();
