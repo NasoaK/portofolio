b0ba08cb5ae80e105785289dcaa85b919157526c
Mikael Kombia <mikael.kombia@gmail.com>
2021-07-18 14:20:37 +0200

    Update about.js

--- public/script/about.js
+++ public/script/about.js
@@ -39,7 +39,9 @@ let click_about = 0;
     if(click_about%2){
         img_about.classList.add('about-img-effect');
         txt_about.classList.add('about-text-effect');
-        div_txt[0].style.marginTop = "-75%";
+        if(window.matchMedia('(max-width:700px)').matches){
+            div_txt[0].style.marginTop = "-75%";
+        }
         console.log('effect');
     }else{
         img_about.classList.remove('about-img-effect');
